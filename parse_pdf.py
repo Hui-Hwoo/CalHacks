2a4791620952d9ab01ed2136254426ffadc9850b
Hui Hwoo <hui.hwoo@gmail.com>
2024-06-23 11:43:36 -0700

    Add parse_pdf

--- parse_pdf.py
+++ parse_pdf.py
@@ -1,6 +1,8 @@
 from PyPDF2 import PdfReader
 from openai import OpenAI
 
+from anki.utils import invoke
+
 from dotenv import load_dotenv
 import os
 import json
@@ -8,7 +10,7 @@ import json
 load_dotenv()
 
 
-def extract_text_from_pdf(pdf_path="../sample.pdf"):
+def extract_text_from_pdf(pdf_path="./sample.pdf"):
     reader = PdfReader(pdf_path)
     number_of_pages = len(reader.pages)
     n = max(7, number_of_pages)
@@ -49,12 +51,26 @@ def fetch_QAs_from_text(text):
 
     message = completion.choices[0].message
     parsed = json.loads(message.content.strip("```").lstrip("json"))
-    print(len(parsed["pairs"]))
+    print(parsed["pairs"])
+    return parsed["pairs"]
 
 
 def main():
     text = extract_text_from_pdf()
-    fetch_QAs_from_text(text[:10000])
+    QAs = fetch_QAs_from_text(text[:10000])
+
+    current_deck = {"name": "CalHacks", "id": 1719082606356}
+    for QA in QAs:
+        Question = QA["Question"]
+        Answer = QA["Answer"]
+        invoke(
+            "addNote",
+            note={
+                "deckName": current_deck["name"],
+                "modelName": "CalHacks",
+                "fields": {"Question": Question, "Answer": Answer},
+            },
+        )
 
 
 if __name__ == "__main__":
